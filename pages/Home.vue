fdf64d5ec63ae29360f48408d52c91a1ab89dedb
Retromac <konitopoulos@hellenictechnologies.com>
2022-06-20 10:07:42 +0300

    changes

--- pages/Home.vue
+++ pages/Home.vue
@@ -1,21 +1,29 @@
 <template>
-<div class="slider">
-    <div class="h-14 bg-gradient-to-r from-sky-500 to-indigo-500 p-[15%]"></div>
-</div>
-<section class="bg-blueW">
-    <div class="box-container flex justify-between w-full max-w-box mx-auto items-center">
-        <div class="col">
-            <h1 class="text-4xl">Φίλτρα νερού βρύσης Camelot<sup>®</sup> για καθαρό φιλτραρισμένο νερό</h1>
-            <p class="text-xl">Τα <b>φίλτρα νερού βρύσης Camelot®</b> σας παρέχουν καθαρό φιλτραρισμένο νερό για να πίνετε, να μαγειρεύετε και να πλένετε τα τρόφιμα σας για έναν ολόκληρο χρόνο. Τα αξιόπιστα και πιστοποιημένα <b>φίλτρα νερού</b> ενεργού άνθρακα με εξαιρετικές κριτικές από χιλιάδες πελάτες μας από την Αθήνα, τη Θεσσαλονίκη και όλη την Ελλάδα, σας προσφέρουν την ιδανική λύση για οικιακή ή επαγγελματική χρήση φίλτρων νερού βρύσης.</p>
-            <button></button>
-        </div>
-        <div class="col">
-            <img src="../assets/images/badge_usa.svg" alt="">
-            <img src="../assets/images/imperial-plus.webp" alt="">
-        </div>
+    <div class="slider">
+        <div class="h-14 bg-gradient-to-r from-sky-500 to-indigo-500 p-[15%]"></div>
     </div>
-    
-</section>
+    <section class="bg-blueW">
+        <div class="box-container flex justify-between w-full max-w-box mx-auto items-center">
+            <div class="col basis-2/4 px-3.5">
+                <h1 class="text-4xl">Φίλτρα νερού βρύσης Camelot<sup>®</sup> για καθαρό φιλτραρισμένο νερό</h1>
+                <p class="text-xl">Τα <b>φίλτρα νερού βρύσης Camelot®</b> σας παρέχουν καθαρό φιλτραρισμένο νερό για να
+                    πίνετε, να μαγειρεύετε και να πλένετε τα τρόφιμα σας για έναν ολόκληρο χρόνο. Τα αξιόπιστα και
+                    πιστοποιημένα <b>φίλτρα νερού</b> ενεργού άνθρακα με εξαιρετικές κριτικές από χιλιάδες πελάτες μας
+                    από την Αθήνα, τη Θεσσαλονίκη και όλη την Ελλάδα, σας προσφέρουν την ιδανική λύση για οικιακή ή
+                    επαγγελματική χρήση φίλτρων νερού βρύσης.</p>
+                <button></button>
+            </div>
+            <div class="col basis-2/4 bg-white">
+                <img src="../assets/images/badge_usa.svg" alt="">
+                <img src="../assets/images/imperial-plus.webp" alt="">
+                <p class="text-blueL text-3xl text-center font-semibold	px-3">
+                    Φίλτρα Νερού Βρύσης Imperial Plus 5ης γενιάς
+                </p>
+                <div class="border_box bg-gradient-to-r from-cyan-500 to-blue-500 p-1 width-full"></div>
+            </div>
+        </div>
+
+    </section>
 </template>
 
 <script>
@@ -25,5 +33,4 @@ export default {
 </script>
 
 <style>
-
 </style>
