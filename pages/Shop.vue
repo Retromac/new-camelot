1243d38cfb5e74895e3681f5408fef7032076da1
Retromac <retrodev8@gmail.com>
2022-06-22 20:38:36 +0300

    shop page

--- pages/Shop.vue
+++ pages/Shop.vue
@@ -1,6 +1,72 @@
 <template>
-  Shop page
-</template>
+<section class="">
+        <div class="box-container flex justify-between w-full max-w-box mx-auto flex-wrap">
+          <div class="text-4xl text-blue font-semibold">
+           <div>
+              Κατάστημα
+           </div> 
+            <img src="../assets/images/icon_wave-titles.svg" alt="wave" class="mt-2">
+          </div>
+          <div>
+           <p>Φίλτρα Νερού Βρύσης, Δωρεάν Παράδοση σε Ελλάδα και Κύπρο. Τα καλύτερα Φίλτρα Νερού σε Ελλάδα και Κύπρο με την εγγύηση της Camelot.</p> 
+           <p>40 χρόνια Νο1 σε Ελλάδα και Κύπρο.</p> 
+           <p>Ο Διεθνής Οργανισμός Υγείας Camelot από το 1980 είναι μέλος μεγάλων διεθνών οργανισμών και αμερικάνικων εργοστασίων. Παρέχουμε λύσεις για καθαρό νερό από το 1988 στην Ελλάδα και στην Κύπρο. Η ανάπτυξη της Camelot στηρίζεται στις συνεχείς προσπάθειες της για την δημιουργία ποιοτικών προϊόντων σχετικών με την υγεία, στην διατήρηση στα τελειότερα επίπεδα ενός εξαιρετικού service πελατών, την συνεχόμενη προσφορά εμπειριών, γνώσεων και ευκαιριών στους συνεργάτες της.</p> 
+           <p>Χωρίς Μόλυβδο, Χωρίς Φυτοφάρμακα, Χωρίς Χλώριο, Χωρίς Ίνες Αμιάντου, Χωρίς Πολυκυκλικούς Αρωματικούς Υδρογονάνθρακες.</p> 
+           <p>Δωρεάν παράδοση και εγκατάσταση άνω πάγκου στο 99% των περιοχών της Ελλάδας και στο 100% των περιοχών της Κύπρου. Τα συστήματα καθαρισμού του νερού μας έχουν πιστοποιήσεις από τους μεγαλύτερους διεθνείς φορείς όπως το NSF, το Water Quality Association της Αμερικής κλπ. Καθώς έχουμε και από Ελληνικούς όπως το Αριστοτέλειο Πανεπιστήμιο Θεσσαλονίκης, το Ινστιτούτο Γεωλογικών και Μεταλλευτικών ερευνών κλπ. Στις περιοχές που έχουμε αρμοδίους η τοποθέτηση είναι δωρεάν. Επίσης σας κάνουν δωρεάν εξέταση αφαίρεσης χλωρίου πριν και μετά την τοποθέτηση του φίλτρου ή της αλλαγής του ανταλλακτικού.</p> 
+           <p>Πριν επιλέξετε κάποιο φίλτρο από εμάς ή από αλλού διαβάστε και ποια είναι τα σημεία που πρέπει να προσέξετε όταν είναι να επιλέξετε ένα σύστημα καθαρισμού νερού καθώς και διαβάστε και τις συχνές ερωτήσεις. </p> 
+
+          </div>
+        </div>
+       </section>
+       <section>
+          <div class="box-container flex w-full max-w-box mx-auto mt-9">
+            <div class="sidebar bg-yellowS px-5 py-10 flex w-4/12">
+              <div class="flex flex-col gap-y-2 text-footer-gray2">
+                  <a href="">Ολα Τα Προϊόντα</a>
+                  <a href="">Ανταλλακτικά Φίλτρα</a>
+                  <a>Ανταλλακτικά Φίλτρα Αέρα</a>
+                  <a href="">Ανταλλακτικά Φίλτρα Μπάνιου</a>
+                  <a href="">Ανταλλακτικά Φίλτρα Μπουκάλι</a>
+                  <a href="">Ανταλλακτικά Φίλτρα Νερού</a>
+                  <a href="">Ανταλλακτικά Φίλτρα Ψύκτη</a>
+                  <a href="">Μπουκάλι Νερού Με Φίλτρο</a>
+                  <a href="">Πλυντήρια Καθαρισμού Τροφίμων</a>
+                  <a href="">Συσκευές Σπιτιού</a>
+                  <a href="">Συσκευή Όζοντος / Οζονοποιητές Νερού</a>
+                  <a href="">Φίλτρα Αέρα - Ιονιστής - Απολυμαντής</a>
+                  <a href="">Φίλτρα Ενεργού Φαρμακευτικού Άνθρακα</a>
+                  <a href="">Φίλτρα Νερού Βρύσης</a>
+                  <a href="">Φίλτρο Νερού Μπάνιου - Ντους - Νιπτήρα</a>
+                  <a href="">Ψύκτες Νερού</a>
+              </div>
+            </div>
+            <div class="shop_content flex w-2/3">
+              <div class="product_grid flex w-full flex-wrap">
+                <div class="product_card flex flex-wrap border-stone-300 border-solid border p-5 w-1/3">
+                  <img src="../assets/images/imperial-plus.webp" alt="">
+                  <div class="ptitle w-full text-center text-blueXL">Φίλτρο Νερού Βρύσης Άνω πάγκου Imperial Plus</div>
+                  <div class="tags w-full  text-center text-gray-500 text-[12px] my-2 pb-2 border-gray-600 border-solid">ΦΙΛΤΡΑ ΕΝΕΡΓΟΥ ΦΑΡΜΑΚΕΥΤΙΚΟΥ ΑΝΘΡΑΚΑ, ΦΙΛΤΡΑ ΝΕΡΟΥ ΒΡΥΣΗΣ</div>
+                  <div class="pprice w-full text-center text-blue flex justify-center font-bold	">
+                    <span class="regular_price">89.00€</span>
+                    <div class="price_seperator">-</div>
+                    <span class="sale_price">1,560.00€</span>
+                  </div>
+                  <div class="more_info border-yellow border-solid border-2 flex m-auto justify-center px-2 py-3 w-full">ΛΕΠΤΟΜΕΡΕΙΕΣ   
+                      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 ml-1 text-blueXL" viewBox="0 0 20 20"
+                          fill="currentColor">
+                          <path fill-rule="evenodd"
+                              d="M10.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H5a1 1 0 110-2h7.586l-2.293-2.293a1 1 0 010-1.414z"
+                              clip-rule="evenodd" />
+                      </svg>
+                  </div>
+                </div>
+                
+              </div>
+            </div>
+          </div>
+       </section>
+                
+        </template>
 
 <script>
 export default {
